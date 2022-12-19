bc1eafa3324bcf8c614d785a70a08b75ce173e01
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2022-12-19 15:22:17 -0500

    Agregué cambios a mi repo!

--- ApiServices/settings.py
+++ ApiServices/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vd31+4ttt1ww9r6=9v)c^vq4(#1bx$-f#k33mpcz64hmx3@n0m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
